f083d9730c2f46ecd1d6ba35e6700f24e10ee415
Sam Fortson <sam.fortson@gmail.com>
2019-04-16 00:21:26 -0400

    Add hash router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 //@flow
 
 import React, { Component } from 'react';
-import Map from './map';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import MyMap from './map';
 
 class App extends Component<{}> {
   render() {
-    return <Map />;
+    return (
+      <HashRouter basename="/">
+        <div style={{ height: '100%' }}>
+          <Switch>
+            <Route path="/" exact component={MyMap} />
+            <Route path="/index.html" component={MyMap} />
+          </Switch>
+        </div>
+      </HashRouter>
+    );
   }
 }
 
