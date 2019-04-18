57eb368578d28ba28839b04336e77357a96d5f23
Sam Fortson <sam.fortson@gmail.com>
2019-04-17 23:29:23 -0400

    Fix marker locations

--- src/map.js
+++ src/map.js
@@ -29,9 +29,45 @@ export default class Map extends Component<{}, State> {
     };
   }
 
-  render() {
+  renderMarkers = () => {
     const { zoom } = this.state.viewport;
     const pinHeight = (zoom * 2).toString() + 'px';
+    const leftOffset = -(parseInt(pinHeight) / 2);
+    const topOffset = -parseInt(pinHeight) - 12;
+    if (zoom > 6) {
+      return (
+        <div>
+          <Marker latitude={53.428594} longitude={-9.319193} offsetLeft={leftOffset} offsetTop={topOffset}>
+            <div style={{ fontSize: '12px' }}>Day 1</div>
+            <img src={MapPin} style={{ height: pinHeight }} alt={'Map Pin'} />
+          </Marker>
+          <Marker latitude={53.455881} longitude={-9.540736} offsetLeft={leftOffset} offsetTop={topOffset}>
+            <div style={{ fontSize: '12px' }}>Day 2</div>
+            <img src={MapPin} style={{ height: pinHeight }} alt={'Map Pin'} />
+          </Marker>
+          <Marker latitude={53.520828} longitude={-9.74239} offsetLeft={leftOffset} offsetTop={topOffset}>
+            <div style={{ fontSize: '12px' }}>Day 3</div>
+            <img src={MapPin} style={{ height: pinHeight }} alt={'Map Pin'} />
+          </Marker>
+          <Marker latitude={53.595843} longitude={-9.694259} offsetLeft={leftOffset} offsetTop={topOffset}>
+            <div style={{ fontSize: '12px' }}>Day 4</div>
+            <img src={MapPin} style={{ height: pinHeight }} alt={'Map Pin'} />
+          </Marker>
+          <Marker latitude={53.696062} longitude={-9.616408} offsetLeft={leftOffset} offsetTop={topOffset}>
+            <div style={{ fontSize: '12px' }}>Day 5</div>
+            <img src={MapPin} style={{ height: pinHeight }} alt={'Map Pin'} />
+          </Marker>
+          <Marker latitude={53.801132} longitude={-9.522257} offsetLeft={leftOffset} offsetTop={topOffset}>
+            <div style={{ fontSize: '12px' }}>Day 6</div>
+            <img src={MapPin} style={{ height: pinHeight }} alt={'Map Pin'} />
+          </Marker>
+        </div>
+      );
+    }
+    return <div />;
+  };
+
+  render() {
     return (
       <ReactMapGL
         height={'100%'}
@@ -42,30 +78,7 @@ export default class Map extends Component<{}, State> {
           'pk.eyJ1Ijoic2ZvcnRzb24iLCJhIjoiY2pvZzJwMnp6MGFyczN2cGphbjBrNGV3NCJ9.X6EVSy5ipogfXTf9DxxjmQ'
         }
       >
-        <Marker latitude={53.428594} longitude={-9.319193}>
-          <div style={{ fontSize: '12px' }}>Day 1</div>
-          <img src={MapPin} style={{ height: pinHeight }} alt={'Map Pin'} />
-        </Marker>
-        <Marker latitude={53.455881} longitude={-9.540736}>
-          <div style={{ fontSize: '12px' }}>Day 2</div>
-          <img src={MapPin} style={{ height: pinHeight }} alt={'Map Pin'} />
-        </Marker>
-        <Marker latitude={53.520828} longitude={-9.74239}>
-          <div style={{ fontSize: '12px' }}>Day 3</div>
-          <img src={MapPin} style={{ height: pinHeight }} alt={'Map Pin'} />
-        </Marker>
-        <Marker latitude={53.595843} longitude={-9.694259}>
-          <div style={{ fontSize: '12px' }}>Day 4</div>
-          <img src={MapPin} style={{ height: pinHeight }} alt={'Map Pin'} />
-        </Marker>
-        <Marker latitude={53.696062} longitude={-9.616408}>
-          <div style={{ fontSize: '12px' }}>Day 5</div>
-          <img src={MapPin} style={{ height: pinHeight }} alt={'Map Pin'} />
-        </Marker>
-        <Marker latitude={53.801132} longitude={-9.522257}>
-          <div style={{ fontSize: '12px' }}>Day 6</div>
-          <img src={MapPin} style={{ height: pinHeight }} alt={'Map Pin'} />
-        </Marker>
+        {this.renderMarkers()}
       </ReactMapGL>
     );
   }
